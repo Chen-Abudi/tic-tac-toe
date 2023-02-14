96732a5b5c7e1584944b5964b3e4dbbd3edef3ba
Tamer <tamerjb.98@gmail.com>
2023-02-14 19:14:24 +0200

    process env code fix

--- packages/server/index.js
+++ packages/server/index.js
@@ -3,12 +3,9 @@ const mongoose = require('mongoose');
 const app = express();
 require('dotenv').config();
 
-const { MONGODB_URI = 'mongodb://localhost:27017/tictac' } = process.env;
+const { MONGODB_URI = 'mongodb://localhost:27017/tictac',PORT = 3000  } = process.env;
 mongoose.connect(MONGODB_URI);
 
-
-
-const { PORT = 3000 } = process.env;
 app.use(express.json());
 
     app.listen(PORT, () => {
