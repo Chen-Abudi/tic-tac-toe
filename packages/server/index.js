f6b603ee30f5a4f8d039fcd4fc6ac31d091ac3fd
Tamer <tamerjb.98@gmail.com>
2023-03-02 21:56:35 +0200

    socket work

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { Socket } = require('socket.io');
 const app = express();
 require('dotenv').config();
+const io = require('socket.io')(3002, {
+  cors: {
+    origin: ['http://localhost:3001'],
+  },
+});
 
-const { MONGODB_URI = 'mongodb://localhost:27017/tictac',PORT = 3000  } = process.env;
+const { MONGODB_URI = 'mongodb://localhost:27017/tictac', PORT = 3000 } =
+  process.env;
 mongoose.connect(MONGODB_URI);
+
+io.on('connection', (Socket) => {
+  console.log(Socket.id);
+});
 
 app.use(express.json());
 
-    app.listen(PORT, () => {
-      console.log(`App listening on port ${PORT}`);
-    });
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
